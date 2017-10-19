e4bc4c5cc9542eb8aeb4ee1c801dd88ca9720f50
osadderp <32919721+osadderp@users.noreply.github.com>
2017-10-19 09:08:25 -0500

    Add files via upload
    Fuentes de un servicio Wcf base que suma dos números enteros.

---
+++ WcfServiceLibraryCalculadora/WcfServiceLibraryCalculadora/IServiceCalculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibraryCalculadora
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IServiceCalculadora
+	{
+		[OperationContract]
+		int suma(int num1, int num2);
+	}
+}
---
+++ WcfServiceLibraryCalculadora/WcfServiceLibraryCalculadora/ServiceCalculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibraryCalculadora
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+	public class ServiceCalculadora : IServiceCalculadora
+	{
+		public int suma(int num1, int num2)
+		{
+			int res;
+
+			res = num1 + num2;
+			return res;
+		}
+	}
+}
