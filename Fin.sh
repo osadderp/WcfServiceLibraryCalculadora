1679252837c017f5681ee87fe78746ace178d3f3
osadderp <32919721+osadderp@users.noreply.github.com>
2017-10-19 09:10:40 -0500

    Add files via upload
    Archivo Inicio y fin para ejecutar el servicio en Docker.

---
+++ Fin.sh
@@ -0,0 +1,3 @@
+docker stop $(docker ps -a -q)
+docker rm wcftallerdockercalculadora
+docker rmi image wcftallerdockercalculadora
---
+++ Inicio.sh
@@ -0,0 +1,1 @@
+docker run -d -p 83:83 --name my-running-site wcftallerdockercalculadora
