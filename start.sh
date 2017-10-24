76aba2bc3b08af5507345cb8935815e2143e6805
osadderp <32919721+osadderp@users.noreply.github.com>
2017-10-23 21:00:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker pull osadder/wcftallerdockercalculadora
-docker run -d -p 83:83 --name my-running-site wcftallerdockercalculadora
+docker run -d -p 83:83 --name my-running-site osadder/wcftallerdockercalculadora
